913f464a0ee0909e33203d5c47995b1901606b61
ruty-hazeh <rutyh1828@gmail.com>
2025-07-18 10:24:09 +0300

    k

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,19 +1,88 @@
-pipeline{
+//pipeline{
+//    agent { label 'verisoft-2' }
+//
+//    environment{
+//        PROJECT_NAME = 'myFirstProject'
+//
+//    }
+//    parameters{
+//        choice(choices: ["cake", "bread", "milk"], name: "Food")
+//        string(defaultValue: "Yshai", name: "lastName", description: "myName")
+//    }
+//    options{
+//        skipStagesAfterUnstable()
+//    }
+//    stage{
+//        stage('print parameters') {
+//            steps {
+//                echo "parameters ${params.Food}"
+//                echo "parameters ${params}"
+//                echo "environment variable ${env.PROJECT_NAME}"
+//                echo "${env.BUILD_NAME}"
+//            }
+//        }
+//        stage('abort if true') {
+//            when {
+//                expression {
+//                    return params.isTrue
+//                }
+//            }
+//            steps {
+//                script {
+//                    currentBuild.result = 'UNSTABLE'
+//                }
+//            }
+//        }
+//        stage('input') {
+//            steps {
+//                input(message: "click if to continue run", ok: "ok")
+//            }
+//        }
+//        stage('Build') {
+//            steps {
+//                script {
+//                    echo 'Building ...'
+//
+//                }
+//            }
+//
+//        }
+//
+//        stage('Test') {
+//
+//            steps {
+//                echo 'Testing ... '
+//            }
+//        }
+//        stage('Deploy') {
+//            steps {
+//                echo 'Deploying ... '
+//
+//            }
+//        }
+//
+//    }
+//}
+
+
+pipeline {
     agent { label 'verisoft-2' }
 
-    environment{
+    environment {
         PROJECT_NAME = 'myFirstProject'
-
     }
-    parameters{
+
+    parameters {
         choice(choices: ["cake", "bread", "milk"], name: "Food")
         string(defaultValue: "Yshai", name: "lastName", description: "myName")
     }
-    options{
+
+    options {
         skipStagesAfterUnstable()
-        disableConcurrentBuild()
+        disableConcurrentBuilds()  // שינוי כאן
     }
-    stage{
+
+    stages {
         stage('print parameters') {
             steps {
                 echo "parameters ${params.Food}"
@@ -22,6 +91,7 @@ pipeline{
                 echo "${env.BUILD_NAME}"
             }
         }
+
         stage('abort if true') {
             when {
                 expression {
@@ -34,33 +104,31 @@ pipeline{
                 }
             }
         }
+
         stage('input') {
             steps {
                 input(message: "click if to continue run", ok: "ok")
             }
         }
+
         stage('Build') {
             steps {
                 script {
                     echo 'Building ...'
-
                 }
             }
-
         }
 
         stage('Test') {
-
             steps {
-                echo 'Testing ... '
+                echo 'Testing ...'
             }
         }
+
         stage('Deploy') {
             steps {
-                echo 'Deploying ... '
-
+                echo 'Deploying ...'
             }
         }
-
     }
 }
